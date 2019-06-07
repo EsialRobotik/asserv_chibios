00d642c43bac700da905257f2886c003c0d17547
jguyonneau <jguyonneau@gmail.com>
2019-06-07 12:47:37 +0200

    Dissociate regulator feedback update & new output compute

--- Regulator.cpp
+++ Regulator.cpp
@@ -7,17 +7,30 @@
 
 #include "Regulator.h"
 
-Regulator::Regulator(float Kp)
+Regulator::Regulator(float Kp, float max_output)
 {
 	m_accumulator = 0;
 	m_Kp = Kp;
 	m_error = 0;
+	m_output = 0;
+	m_maxOutput = max_output;
 }
 
-float Regulator::update(float goal, float feedback)
+void Regulator::updateFeedback(float feedback)
 {
 	m_accumulator += feedback;
-	m_error = goal-m_accumulator;
-	return m_error*m_Kp;
 }
 
+
+float Regulator::updateOutput(float goal)
+{
+	m_error = goal-m_accumulator;
+	m_output =  m_error*m_Kp;
+
+	if(m_output < -m_maxOutput)
+		m_output = -m_maxOutput;
+	else if(m_output > m_maxOutput)
+		m_output = m_maxOutput;
+
+	return m_output;
+}
--- Regulator.h
+++ Regulator.h
@@ -13,23 +13,29 @@
 class Regulator
 {
 public:
-	explicit Regulator(float Kp);
+	explicit Regulator(float Kp, float max_output);
 	virtual ~Regulator(){};
 
-	float update(float goal, float feedback);
+	void updateFeedback(float feedback);
+	float updateOutput(float goal);
 
-	float getAccumulator(){ return m_accumulator;};
+	float getAccumulator() const { return m_accumulator;};
 
 	void setGain(float Kp){ m_Kp = Kp;}
+	float getGain() const { return m_Kp;};
+
 
 	void reset(){m_accumulator = 0;};
 
-	float getError(){ return m_error;};
+	float getError() const { return m_error;};
+	float getOutput() const { return m_output;};
 
 private:
     double m_accumulator;
     float m_Kp;
     float m_error;
+    float m_output;
+    float m_maxOutput;
 
 };
 
