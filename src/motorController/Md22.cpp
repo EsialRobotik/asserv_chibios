713f4ff58d0b9718298a25cd949c3e18c87f7c96
pmx <pmx@pmx-vm>
2020-04-12 15:28:35 +0200

    conflit géré.

--- src/motorController/Md22.cpp
+++ src/motorController/Md22.cpp
@@ -3,39 +3,33 @@
 #include <hal.h>
 #include "util/asservMath.h"
 
-/*
- static const I2CConfig i2cconfig100kHz = { OPMODE_I2C, 100000, STD_DUTY_CYCLE };
- static const I2CConfig i2cconfig400kHz = { OPMODE_I2C, 400000, FAST_DUTY_CYCLE_2 };
+constexpr uint8_t md22Address = 0xB0 >> 1; // MD22 address (All switches to ON) 0x10110000 =>1011000 0x58
+constexpr uint8_t modeReg = 0x00;
+constexpr uint8_t motor1Reg = 0x01;
+constexpr uint8_t motor2Reg = 0x02;
+constexpr uint8_t accReg = 0x03;
 
- constexpr uint8_t md22Address = 0xB0 >> 1; // MD22 address (All switches to ON) 0x10110000 =>1011000 0x58
- constexpr uint8_t modeReg = 0x00;
- constexpr uint8_t motor1Reg = 0x01;
- constexpr uint8_t motor2Reg = 0x02;
- constexpr uint8_t accReg = 0x03;
+constexpr uint8_t controlMode = 0x01; // Wanted value for mode register. Ie: -128 (full reverse)   0 (stop)   127 (full forward).
 
- constexpr uint8_t controlMode = 0x01; // Wanted value for mode register. Ie: -128 (full reverse)   0 (stop)   127 (full forward).
- */
-Md22::Md22(bool is1motorRight, bool invertMotorRight, bool invertMotorLeft, I2cPinInit pins, int freq) :
+ Md22::Md22(bool is1motorRight, bool invertMotorRight, bool invertMotorLeft, I2cPinInit *i2cPins, uint32_t i2cFrequency) :
         MotorController()
 {
-    m_i2cPinConf = pins;
+    m_i2cPinConf = *i2cPins;
     m_invertMotorRight = invertMotorRight;
     m_invertMotorLeft = invertMotorLeft;
     m_is1motorRight = is1motorRight;
-    m_freq = freq;
 
-    if (m_freq > 0 and m_freq <= 100) {
-        m_i2cconfig = { OPMODE_I2C, 100000, STD_DUTY_CYCLE };
-    }else if (m_freq > 100 and m_freq <= 400) {
-        m_i2cconfig = { OPMODE_I2C, 400000, FAST_DUTY_CYCLE_2 };
-    }else
-        m_i2cconfig = { OPMODE_I2C, 100000, STD_DUTY_CYCLE };
+
+    chDbgAssert(i2cFrequency <= 400000, "Md22: i2cFrequency shall be lower than 400khz \r\n");
+
+    if (i2cFrequency <= 100000)
+        m_i2cconfig = { OPMODE_I2C, i2cFrequency, STD_DUTY_CYCLE };
+    else if (i2cFrequency > 100000)
+        m_i2cconfig = { OPMODE_I2C, i2cFrequency, FAST_DUTY_CYCLE_2 };
 }
 
 void Md22::init()
 {
-    sysinterval_t tmo = TIME_MS2I(4);
-
     // Enable I2C SDA & SCL pin
     // External pullups with correct resistance value shall be used !
     // see : http://wiki.chibios.org/dokuwiki/doku.php?id=chibios:community:guides:i2c_trouble_shooting
@@ -48,27 +42,29 @@ void Md22::init()
 
     i2cAcquireBus (&I2CD1);
 
+    sysinterval_t tmo = TIME_MS2I(4);
+
     // Set mode
-    uint8_t cmd[] = { MD22_MODEREG, MD22_CONTROLMODE };
-    msg_t msg = i2cMasterTransmitTimeout(&I2CD1, MD22_ADDRESS, cmd, sizeof(cmd), NULL, 0, tmo);
+    uint8_t cmd[] = { modeReg, controlMode };
+    msg_t msg = i2cMasterTransmitTimeout(&I2CD1, md22Address, cmd, sizeof(cmd), NULL, 0, tmo);
 
     chDbgAssert(msg == MSG_OK, "Config MD22 - i2cMasterTransmitTimeout ERROR NOK\r\n");
 
     // Set acceleration
-    cmd[0] = MD22_ACCREG;
+    cmd[0] = accReg;
     cmd[1] = 0;
-    msg = i2cMasterTransmitTimeout(&I2CD1, MD22_ADDRESS, cmd, sizeof(cmd), NULL, 0, tmo);
+    msg = i2cMasterTransmitTimeout(&I2CD1, md22Address, cmd, sizeof(cmd), NULL, 0, tmo);
     chDbgAssert(msg == MSG_OK, "Config MD22 - i2cMasterTransmitTimeout ERROR NOK\r\n");
 
     // Set motor speed to zero
-    cmd[0] = MD22_MOTOR1REG;
+    cmd[0] = motor1Reg;
     cmd[1] = 0;
-    msg = i2cMasterTransmitTimeout(&I2CD1, MD22_ADDRESS, cmd, sizeof(cmd), NULL, 0, tmo);
+    msg = i2cMasterTransmitTimeout(&I2CD1, md22Address, cmd, sizeof(cmd), NULL, 0, tmo);
     chDbgAssert(msg == MSG_OK, "Config MD22 - i2cMasterTransmitTimeout ERROR NOK\r\n");
 
-    cmd[0] = MD22_MOTOR2REG;
+    cmd[0] = motor2Reg;
     cmd[1] = 0;
-    msg = i2cMasterTransmitTimeout(&I2CD1, MD22_ADDRESS, cmd, sizeof(cmd), NULL, 0, tmo);
+    msg = i2cMasterTransmitTimeout(&I2CD1, md22Address, cmd, sizeof(cmd), NULL, 0, tmo);
     chDbgAssert(msg == MSG_OK, "Config MD22 - i2cMasterTransmitTimeout ERROR NOK\r\n");
 
     i2cReleaseBus(&I2CD1);
@@ -82,15 +78,15 @@ void Md22::setMotorLeftSpeed(float percentage)
         percentage = -percentage;
 
     int8_t md22SpeedConsign = (int8_t) fmap(percentage, -100.0, 100.0, -128.0, 127.0);
-    uint8_t reg = 0;
+    uint8_t reg ;
     if (m_is1motorRight)
-        reg = MD22_MOTOR2REG;
+        reg = motor2Reg;
     else
-        reg = MD22_MOTOR1REG;
+        reg = motor1Reg;
 
     uint8_t cmd[] = { reg, (uint8_t) md22SpeedConsign };
     i2cAcquireBus (&I2CD1);
-    i2cMasterTransmitTimeout(&I2CD1, MD22_ADDRESS, cmd, sizeof(cmd), NULL, 0, TIME_MS2I(1));
+    i2cMasterTransmitTimeout(&I2CD1, md22Address, cmd, sizeof(cmd), NULL, 0, TIME_MS2I(1));
     i2cReleaseBus(&I2CD1);
 }
 
@@ -102,15 +98,15 @@ void Md22::setMotorRightSpeed(float percentage)
         percentage = -percentage;
 
     int8_t md22SpeedConsign = (int8_t) fmap(percentage, -100.0, 100.0, -128.0, 127.0);
-    uint8_t reg = 0;
+    uint8_t reg;
     if (m_is1motorRight)
-        reg = MD22_MOTOR1REG;
+        reg = motor1Reg;
     else
-        reg = MD22_MOTOR2REG;
+        reg = motor2Reg;
 
     uint8_t cmd[] = { reg, (uint8_t) md22SpeedConsign };
     i2cAcquireBus (&I2CD1);
-    i2cMasterTransmitTimeout(&I2CD1, MD22_ADDRESS, cmd, sizeof(cmd), NULL, 0, TIME_MS2I(1));
+    i2cMasterTransmitTimeout(&I2CD1, md22Address, cmd, sizeof(cmd), NULL, 0, TIME_MS2I(1));
     i2cReleaseBus(&I2CD1);
 }
 
