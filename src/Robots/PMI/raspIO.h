1a85b59b7bc89080fffdfc337935f4470b4ac51b
GaG <francois.prugniel@telecomnancy.net>
2022-05-18 20:59:58 +0200

    Fix PMI raspIO

--- src/Robots/PMI/raspIO.h
+++ src/Robots/PMI/raspIO.h
@@ -1,1 +1,10 @@
-../Princess/raspIO.h
+
+#ifndef SRC_ROBOTS_PRINCESS_RASPIO_H_
+#define SRC_ROBOTS_PRINCESS_RASPIO_H_
+
+
+extern THD_FUNCTION(asservCommandSerial, p);
+extern THD_FUNCTION(asservPositionSerial, p);
+
+
+#endif /* SRC_ROBOTS_PRINCESS_RASPIO_H_ */
